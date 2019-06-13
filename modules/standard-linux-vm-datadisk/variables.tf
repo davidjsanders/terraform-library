9de2746150d339b2016b1363411441c5cb42b8b0
David Sanders <dsanderscanada@gmail.com>
2019-06-12 20:44:53 -0400

    Add standard-linux-vm-datadisk

---
+++ modules/standard-linux-vm-datadisk/variables.tf
@@ -0,0 +1,50 @@
+variable "name" {}
+variable "resource-group-name" {}
+variable "nic-id" {}
+variable "vm-size" {}
+variable "delete-os-disk-on-termination" {}
+variable "delete-data-disks-on-termination" {}
+variable "availability-set-id" {}
+variable "primary-blob-endpoint" {}
+variable "image-publisher" {}
+variable "image-offer" {}
+variable "image-sku" {}
+variable "image-version" {}
+variable "os-disk-name" {}
+variable "os-disk-caching" {}
+variable "os-disk-create-option" {}
+variable "os-disk-type" {}
+variable "data-disk-name" {}
+variable "data-disk-id" {}
+variable "data-disk-create-option" {
+  default = "Attach"
+}
+variable "data-disk-lun" {
+  default = 1
+}
+variable "data-disk-size" {}
+
+variable "adminuser" {
+  default = "azadmin"
+}
+
+variable "adminpass" {
+  default = "Password1234!"
+}
+
+variable "custom-data" {}
+variable "disable-password-auth" {}
+
+variable "ssh-key-path" {
+  default = ""
+}
+
+variable "location" {}
+variable "tags" {
+    type = "map"
+    default = {
+        tag_description = "!!Not Provided!!"
+        tag_billing = "!!Not Provided!!"
+        tag_environment = "!!Not Provided!!"
+    }
+}
